039388ea853826e023698014a3e9e90a4d83f424
Gabriel <gmonteiro810@gmail.com>
2025-03-16 19:24:22 -0300

    Refactor reservation handling and error management
    - Removed folder reference for `Src\Services\` in `venue_service.csproj`.
    - Added `HttpResponseException` class for improved error handling in `ExceptionErrorDto.cs`.
    - Introduced `IReservationService` interface with methods for creating and retrieving reservations in `IReservationService.cs`.
    - Implemented `ReservationService` class in `ReservationService.cs`, including logic for validating user and venue existence during reservation creation.

---
+++ venue_service/Src/Services/IReservationService.cs
@@ -0,0 +1,11 @@
+﻿using venue_service.Src.Dtos;
+using venue_service.Src.Dtos.Reservation;
+
+namespace venue_service.Src.Services
+{
+    public interface IReservationService
+    {
+        Task<ReservationResponseDto> CreateReservationAsync(CreateReservationDto dto);
+        Task<IEnumerable<ReservationResponseDto>> GetReservationsByUserIdAsync(int userId);
+    }
+}
---
+++ venue_service/Src/Services/ReservationService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using venue_service.Src.Contexts;
+using venue_service.Src.Dtos;
+using venue_service.Src.Dtos.Reservation;
+
+namespace venue_service.Src.Services
+{
+    public class ReservationService : IReservationService
+    {
+        private readonly DatabaseContext _context;
+
+        public ReservationService(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ReservationResponseDto> CreateReservationAsync(CreateReservationDto dto)
+        {
+
+            var userExists = await _context.Users.AnyAsync(u => u.Id == dto.UserId);
+            var VenueExists = await _context.Venues.AnyAsync(v => v.Id == dto.VenueId);
+            var availabilityExists = await _context.LocationAvailabilityTimes.AnyAsync(l => l.Id == dto.LocationAvailabilityTimeId);
+            var paymentMethodExists = await _context.PaymentMethods.AnyAsync(pm => pm.Id == dto.PaymentMethodId);
+
+            if (!userExists) throw new Exception("User not found") { Data = { { "ErrorHttpNumber", 404 } } };
+        }
+    }
+}
