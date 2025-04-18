046381cc98cd1b68deb2fcad5a17d4f2e5766940
Gabriel <gmonteiro810@gmail.com>
2025-04-18 15:41:47 -0300

    Add AuthResponseDto class for authentication responses
    Introduced a new class `AuthResponseDto` in the
    `venue_service.Src.Dtos.Auth` namespace. This class
    includes three string properties: `Token`, `Email`,
    and `FirstName`, to encapsulate authentication response
    data in a structured format.

---
+++ venue_service/Src/Dtos/Auth/AuthResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace venue_service.Src.Dtos.Auth;
+
+public class AuthResponseDto
+{
+    public string Token { get; set; }
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+}
