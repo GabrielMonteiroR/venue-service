bb8a42113d467defa8568dc8aa7f211322d2e64f
Gabriel <gmonteiro810@gmail.com>
2025-05-02 16:01:15 -0300

    Improve user retrieval and add update functionality
    Modified the `GetUserInfoByIdAsync` method in `ProfileService.cs` to enhance exception formatting for better readability.
    
    Added a new method, `UpdateUserInfoAsync`, to allow updating user information with error handling for user not found and general exceptions. This method updates user properties and returns the updated user information as a `UserResponseDto`.

--- venue_service/Src/Services/ProfileService.cs
+++ venue_service/Src/Services/ProfileService.cs
@@ -19,7 +19,7 @@ namespace venue_service.Src.Services
             try
             {
                 var user = await _context.Users.FindAsync(id);
-                if (user is null) throw new HttpResponseException(HttpStatusCode.NotFound,"User not found",$"User with id {id} not found.");
+                if (user is null) throw new HttpResponseException(HttpStatusCode.NotFound, "User not found", $"User with id {id} not found.");
 
                 return new UserResponseDto
                 {
@@ -34,6 +34,39 @@ namespace venue_service.Src.Services
             } catch (Exception ex)
             {
                 throw new HttpResponseException(HttpStatusCode.InternalServerError, "An error occurred while retrieving user information.", ex.Message);
+            }
+        }
+
+    public async Task<UserResponseDto> UpdateUserInfoAsync(int id, UserResponseDto userDto)
+        {
+            try
+            {
+                var user = await _context.Users.FindAsync(id);
+                if (user is null) throw new HttpResponseException(HttpStatusCode.NotFound, "User not found", $"User with id {id} not found.");
+                user.FirstName = userDto.FirstName;
+                user.LastName = userDto.LastName;
+                user.Email = userDto.Email;
+                user.Phone = userDto.Phone;
+                user.RoleId = userDto.RoleId;
+                user.IsBanned = userDto.IsBanned;
+
+                _context.Users.Update(user);
+                await _context.SaveChangesAsync();
+
+                return new UserResponseDto
+                {
+                    Id = user.Id,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Email = user.Email,
+                    Phone = user.Phone,
+                    RoleId = user.RoleId,
+                    IsBanned = user.IsBanned
+                };
+            }
+            catch (Exception ex)
+            {
+                throw new HttpResponseException(HttpStatusCode.InternalServerError, "An error occurred while updating user information.", ex.Message);
             }
         }
     }
