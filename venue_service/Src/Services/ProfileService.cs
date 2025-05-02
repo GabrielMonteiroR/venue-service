0bd94fc17e0e0b20cc1081dc36b266a816351583
Gabriel <gmonteiro810@gmail.com>
2025-05-02 15:52:50 -0300

    Add UserResponseDto and ProfileService class
    Introduced a new data transfer object (DTO) `UserResponseDto` with properties for user details in `UserResponseDto.cs`. Added a new service class `ProfileService` in `ProfileService.cs`, which includes a constructor for `DatabaseContext`, but the method implementation is currently incomplete.

---
+++ venue_service/Src/Services/ProfileService.cs
@@ -0,0 +1,16 @@
+﻿using venue_service.Src.Contexts;
+
+namespace venue_service.Src.Services
+{
+    public class ProfileService
+    {
+        private readonly DatabaseContext _context;
+
+        public ProfileService(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<>
+    }
+}
