80135d04513e4a79b1b9d5b87e015ccaaa8d6ba9
Gabriel <gmonteiro810@gmail.com>
2025-05-02 16:03:45 -0300

    Add UserController for user information retrieval
    Introduces a new `UserController` in the `venue_service.Src.Controllers` namespace. This API controller includes a dependency on `ProfileService` and features an asynchronous method `GetUserInfo` to fetch user details by ID from the query string, returning the result as an HTTP response.

---
+++ venue_service/Src/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using venue_service.Src.Services;
+
+namespace venue_service.Src.Controllers
+{
+    [ApiController]
+    [Route("api/user")]
+    public class UserController : ControllerBase
+    {
+        private readonly ProfileService _profileService;
+
+        public UserController(ProfileService profileService)
+        {
+            _profileService = profileService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUserInfo([FromQuery] int id)
+        {
+            var userInfo = await _profileService.GetUserInfoByIdAsync(id);
+            return Ok(userInfo);
+        }
+
+        
+
+
+    }
+}
