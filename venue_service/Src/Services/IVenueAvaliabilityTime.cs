7a28f164b7ae4e424dc548d59ecdf29b8db843c9
Gabriel <gmonteiro810@gmail.com>
2025-05-04 10:03:02 -0300

    Add update method to IVenueAvaliabilityTime interface
    Introduced a new method `UpdateAvaliabilityTime` to the
    IVenueAvaliabilityTime interface for updating venue
    availability times. Added a using directive for
    `venue_service.Src.Models` to include the
    `VenueAvailabilityTime` model.

--- venue_service/Src/Services/IVenueAvaliabilityTime.cs
+++ venue_service/Src/Services/IVenueAvaliabilityTime.cs
@@ -1,4 +1,5 @@
 ﻿using venue_service.Src.Dtos;
+using venue_service.Src.Models;
 
 namespace venue_service.Src.Services
 {
@@ -6,6 +7,7 @@ namespace venue_service.Src.Services
     {
         Task<VenueAvailabilityTimeResponseDto> CreateVenueAvailabilityTimeAsync(CreateVenueAvaliabilityDto dto);
         Task<VenueAvaliabilityTimesResponseDto> ListAvaliableTimesByVenue(int venueId);
+        Task<VenueAvailabilityTime> UpdateAvaliabilityTime(int id, UpdateVenueAvaliabilityDto newTimeDto)
         Task<bool> DeleteVenueAvailabilityTimeAsync(int id);
     }
 }
