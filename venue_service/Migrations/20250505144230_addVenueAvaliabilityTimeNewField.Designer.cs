8f9bc2ae9fabfed49bce40e5fba605698d3a88e5
Gabriel <gmonteiro810@gmail.com>
2025-05-05 12:33:48 -0300

    Add reserved_by column to venue_availability table
    Introduced a new nullable integer column `reserved_by` to the `venue_availability` table to track the user who reserved the venue. Updated migration file `20250505144230_addVenueAvaliabilityTimeNewField.cs` and its designer file, along with modifications to `DatabaseContextModelSnapshot.cs` to reflect this change in the `VenueAvailabilityTime` entity.

--- venue_service/Migrations/20250505144230_addVenueAvaliabilityTimeNewField.Designer.cs
+++ venue_service/Migrations/20250505144230_addVenueAvaliabilityTimeNewField.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using venue_service.Src.Contexts;
@@ -11,9 +12,11 @@ using venue_service.Src.Contexts;
 namespace venue_service.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20250505144230_addVenueAvaliabilityTimeNewField")]
+    partial class addVenueAvaliabilityTimeNewField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
