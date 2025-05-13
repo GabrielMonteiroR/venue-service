cfc46721963798948bd4fcb1dd1d06444195e053
Gabriel <gmonteiro810@gmail.com>
2025-05-13 12:53:44 -0300

    Add UpdateVenueImageResponseDto class
    Introduces a new class `UpdateVenueImageResponseDto` in the `venue_service.Src.Dtos` namespace. This class includes properties for `VenueId`, `NewImageUrls`, and `Message`, facilitating the encapsulation of response data for venue image updates.

---
+++ venue_service/Src/Dtos/UpdateVenueImageResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace venue_service.Src.Dtos;
+
+public class UpdateVenueImageResponseDto
+{
+    public int VenueId { get; set; }
+    public List<string> NewImageUrls { get; set; } = new();
+    public string Message { get; set; } = string.Empty;
+}
