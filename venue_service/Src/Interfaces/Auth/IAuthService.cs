f8c782d03627ba3113b17e9860f8753b63665365
Gabriel <gmonteiro810@gmail.com>
2025-05-19 20:15:44 -0300

    Add new interfaces for authentication and venue management
    This commit introduces several new interfaces in the `venue_service` project under the `Src.Interfaces` namespace. Key additions include:
    
    - `IAuthService` for user authentication, including registration and login methods.
    - `IStorageService` for handling image uploads and deletions.
    - `IReservationService` for managing reservations.
    - `IVenueAvaliabilityTime` for venue availability operations.
    - `IVenueService` for comprehensive venue management.
    - `IVenueType` for retrieving all venue types.
    
    Additionally, the project file `venue_service.csproj` has been updated to remove a folder reference to `Src\Iterfaces\Auth`, and the namespace for all interfaces has been corrected to `venue_service.Src.Interfaces` for consistency.

---
+++ venue_service/Src/Interfaces/Auth/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using venue_service.Src.Dtos.Auth;
+
+namespace venue_service.Src.Interfaces.Auth;
+
+public interface IAuthService
+{
+    Task<AuthResponseDto> RegisterOwnerAsync(RegisterOwnerRequestDto dto);
+    Task<AuthResponseDto> RegisterAthleteAsync(RegisterUserRequestDto dto);
+    AuthResponseDto Login(LoginRequestDto dto);
+}
