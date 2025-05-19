4256bdfce4698c353e186a9d889529a853f48f95
Gabriel <gmonteiro810@gmail.com>
2025-05-19 15:12:55 -0300

    Remove EquipamentBrand and add EquipamentContext
    The `EquipamentBrand` class has been removed from the codebase, eliminating its properties and relationships. A new `EquipamentContext` class has been introduced to manage the database context, featuring `DbSet` properties for `EquipamentType` and `VenueEquipament`. The `OnModelCreating` method is overridden to configure entity mappings and relationships, enhancing the structure for database interactions using Entity Framework Core.

---
+++ venue_service/Src/Contexts/EquipamentContext.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using venue_service.Src.Models;
+
+namespace venue_service.Src.Contexts;
+
+public class EquipamentContext : DbContext
+{
+    public EquipamentContext(DbContextOptions<EquipamentContext> options) : base(options) { }
+
+    public DbSet<EquipamentType> EquipamentTypes => Set<EquipamentType>();
+    public DbSet<VenueEquipament> VenueEquipaments => Set<VenueEquipament>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<EquipamentType>().ToTable("equipament_types");
+        modelBuilder.Entity<VenueEquipament>().ToTable("venue_equipaments");
+
+        modelBuilder.Entity<EquipamentType>()
+            .HasMany(et => et.VenueEquipaments)
+            .WithOne(ve => ve.EquipamentType)
+            .HasForeignKey(ve => ve.EquipamentTypeId);
+
+        modelBuilder.Entity<VenueEquipament>()
+            .HasOne(ve => ve.Venue)
+            .WithMany(v => v.VenueEquipaments)
+            .HasForeignKey(ve => ve.VenueId);
+    }
+}
