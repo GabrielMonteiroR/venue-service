4794f671659459a152a4f755b089646330c7ffaf
Gabriel <gmonteiro810@gmail.com>
2025-05-19 18:30:08 -0300

    Update project dependencies and add user registration DTO
    Removed WeatherForecast.cs from compilation. Added package references for DocsBRValidator, Microsoft.AspNetCore.Authentication.JwtBearer, Microsoft.AspNetCore.Identity, and Microsoft.EntityFrameworkCore.
    
    Introduced RegisterUserRequestDto with validation attributes for user registration, including properties for FirstName, LastName, Email, Image, Password, Phone, and Cpf.

---
+++ venue_service/Src/Dtos/Auth/RegisterOwnerRequestDto.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DocsBRValidator.Attributes;
+
+namespace venue_service.Src.Dtos.Auth;
+
+public class RegisterUserRequestDto
+{
+    [Required(ErrorMessage = "Nome é obrigatório.")]
+    [StringLength(100, MinimumLength = 2)]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Sobrenome é obrigatório.")]
+    [StringLength(100, MinimumLength = 2)]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "Email é obrigatório.")]
+    [EmailAddress(ErrorMessage = "Email inválido.")]
+    public string Email { get; set; }
+
+    public IFormFile? Image { get; set; }
+
+    [Required(ErrorMessage = "Senha é obrigatória.")]
+    [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 6 caracteres.")]
+    public string Password { get; set; }
+
+    [Required(ErrorMessage = "Telefone é obrigatório.")]
+    [RegularExpression(@"^\(?[1-9]{2}\)? ?9?[6-9]\d{3}-?\d{4}$",
+        ErrorMessage = "Telefone inválido para padrão brasileiro.")]
+    public string Phone { get; set; }
+
+    [Required(ErrorMessage = "CPF é obrigatório.")]
+    [CPF(ErrorMessage = "CPF inválido.")]
+    public string Cpf { get; set; }
+}
