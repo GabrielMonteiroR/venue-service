78c1d8e2ea66235bcf7f2016433cf2431d4867e1
Gabriel <gmonteiro810@gmail.com>
2025-05-19 15:35:17 -0300

    Refactor database context and add initial migrations
    - Removed `Venue_Sport` DbSet from `SportContext.cs`.
    - Added migration `20250519183135_InitSport` to create tables for sports-related entities.
    - Generated `InitSport.Designer.cs` and updated `SportContextModelSnapshot.cs`.
    - Added migration `20250519183118_InitUser` for user management tables.
    - Generated `InitUser.Designer.cs` and updated `UserContextModelSnapshot.cs`.
    - Added migration `20250519183110_InitVenue` for venue management tables.
    - Generated `InitVenue.Designer.cs` and updated `VenueContextModelSnapshot.cs`.

---
+++ venue_service/Migrations/Sport/20250519183135_InitSport.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace venue_service.Migrations.Sport
+{
+    /// <inheritdoc />
+    public partial class InitSport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "equipament_types",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    type_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_equipament_types", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "roles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_roles", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "sports",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_sports", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "venue_types",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    description = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_venue_types", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    first_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    last_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    email = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    password = table.Column<string>(type: "text", nullable: false),
+                    ProfileImageUrl = table.Column<string>(type: "text", nullable: true),
+                    phone = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    role_id = table.Column<int>(type: "integer", nullable: false),
+                    is_banned = table.Column<bool>(type: "boolean", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_users_roles_role_id",
+                        column: x => x.role_id,
+                        principalTable: "roles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "venues",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    address = table.Column<string>(type: "character varying(250)", maxLength: 250, nullable: false),
+                    capacity = table.Column<int>(type: "integer", nullable: false),
+                    latitude = table.Column<double>(type: "double precision", nullable: false),
+                    longitude = table.Column<double>(type: "double precision", nullable: false),
+                    description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    allow_local_payment = table.Column<bool>(type: "boolean", nullable: false),
+                    venue_type_id = table.Column<int>(type: "integer", nullable: false),
+                    rules = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    owner_id = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_venues", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_venues_users_owner_id",
+                        column: x => x.owner_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_venues_venue_types_venue_type_id",
+                        column: x => x.venue_type_id,
+                        principalTable: "venue_types",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "venue_availability_times",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    start_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    end_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    venue_id = table.Column<int>(type: "integer", nullable: false),
+                    price = table.Column<decimal>(type: "numeric", nullable: false),
+                    time_status = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    is_reserved = table.Column<bool>(type: "boolean", nullable: false),
+                    reserved_by = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_venue_availability_times", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_venue_availability_times_venues_venue_id",
+                        column: x => x.venue_id,
+                        principalTable: "venues",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "venue_contact_infos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    phone = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    email = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    venue_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_venue_contact_infos", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_venue_contact_infos_venues_venue_id",
+                        column: x => x.venue_id,
+                        principalTable: "venues",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "venue_equipaments",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    equipament_type_id = table.Column<int>(type: "integer", nullable: false),
+                    quantity = table.Column<int>(type: "integer", nullable: false),
+                    venue_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_venue_equipaments", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_venue_equipaments_equipament_types_equipament_type_id",
+                        column: x => x.equipament_type_id,
+                        principalTable: "equipament_types",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_venue_equipaments_venues_venue_id",
+                        column: x => x.venue_id,
+                        principalTable: "venues",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "venue_images",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    file_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    image_url = table.Column<string>(type: "character varying(300)", maxLength: 300, nullable: false),
+                    venue_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_venue_images", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_venue_images_venues_venue_id",
+                        column: x => x.venue_id,
+                        principalTable: "venues",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "venue_sports",
+                columns: table => new
+                {
+                    venue_id = table.Column<int>(type: "integer", nullable: false),
+                    sport_id = table.Column<int>(type: "integer", nullable: false),
+                    SportId1 = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_venue_sports", x => new { x.venue_id, x.sport_id });
+                    table.ForeignKey(
+                        name: "FK_venue_sports_sports_SportId1",
+                        column: x => x.SportId1,
+                        principalTable: "sports",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK_venue_sports_sports_sport_id",
+                        column: x => x.sport_id,
+                        principalTable: "sports",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_venue_sports_venues_venue_id",
+                        column: x => x.venue_id,
+                        principalTable: "venues",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_role_id",
+                table: "users",
+                column: "role_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_venue_availability_times_venue_id",
+                table: "venue_availability_times",
+                column: "venue_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_venue_contact_infos_venue_id",
+                table: "venue_contact_infos",
+                column: "venue_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_venue_equipaments_equipament_type_id",
+                table: "venue_equipaments",
+                column: "equipament_type_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_venue_equipaments_venue_id",
+                table: "venue_equipaments",
+                column: "venue_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_venue_images_venue_id",
+                table: "venue_images",
+                column: "venue_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_venue_sports_sport_id",
+                table: "venue_sports",
+                column: "sport_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_venue_sports_SportId1",
+                table: "venue_sports",
+                column: "SportId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_venues_owner_id",
+                table: "venues",
+                column: "owner_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_venues_venue_type_id",
+                table: "venues",
+                column: "venue_type_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "venue_availability_times");
+
+            migrationBuilder.DropTable(
+                name: "venue_contact_infos");
+
+            migrationBuilder.DropTable(
+                name: "venue_equipaments");
+
+            migrationBuilder.DropTable(
+                name: "venue_images");
+
+            migrationBuilder.DropTable(
+                name: "venue_sports");
+
+            migrationBuilder.DropTable(
+                name: "equipament_types");
+
+            migrationBuilder.DropTable(
+                name: "sports");
+
+            migrationBuilder.DropTable(
+                name: "venues");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "venue_types");
+
+            migrationBuilder.DropTable(
+                name: "roles");
+        }
+    }
+}
--- venue_service/Src/Contexts/SportContext.cs
+++ venue_service/Src/Contexts/SportContext.cs
@@ -9,26 +9,11 @@ public class SportContext : DbContext
     public SportContext(DbContextOptions<SportContext> options) : base(options) { }
 
     public DbSet<Sport> Sports => Set<Sport>();
-    public DbSet<Venue_Sport> VenueSports => Set<Venue_Sport>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.Entity<Sport>().ToTable("sports");
-        modelBuilder.Entity<Venue_Sport>().ToTable("venue_sports");
-
-        modelBuilder.Entity<Venue_Sport>()
-            .HasKey(vs => new { vs.VenueId, vs.SportId });
-
-        modelBuilder.Entity<Venue_Sport>()
-            .HasOne(vs => vs.Venue)
-            .WithMany(v => v.VenueSports)
-            .HasForeignKey(vs => vs.VenueId);
-
-        modelBuilder.Entity<Venue_Sport>()
-            .HasOne(vs => vs.Sport)
-            .WithMany()
-            .HasForeignKey(vs => vs.SportId);
     }
 }
