1967ac0de5d86225e1caf13be35aa6cf3cfb4333
Gabriel <gmonteiro810@gmail.com>
2025-05-21 11:09:42 -0300

    Add user-related properties to CreateReservationDto
    - Introduced `UserId` (int) to associate reservations with users.
    - Added `CardToken` (string) for payment processing.
    - Included `UserEmail` (string) for user identification and communication.

--- venue_service/Src/Dtos/Reservation/CreateReservationDto.cs
+++ venue_service/Src/Dtos/Reservation/CreateReservationDto.cs
@@ -4,5 +4,9 @@ public class CreateReservationDto
 {
     public int VenueId { get; set; }
     public int VenueAvailabilityTimeId { get; set; }
+    public int UserId { get; set; }
     public int PaymentMethodId { get; set; }
+
+    public string CardToken { get; set; }
+    public string UserEmail { get; set; }
 }
