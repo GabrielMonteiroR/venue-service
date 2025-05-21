130758c3c97061908589f72b6327550d58549e5b
Gabriel <gmonteiro810@gmail.com>
2025-05-21 11:58:33 -0300

    Add MercadoPago payment service and update project file
    Removed WeatherForecast.cs from compilation and added
    mercadopago-sdk package reference. Introduced
    MercadoPagoPaymentService class implementing IPaymentService,
    with a method to create payments using the Mercado Pago SDK.

---
+++ venue_service/Src/Services/Payment/MercadoPagoPaymentService.cs
@@ -0,0 +1,34 @@
+﻿using MercadoPago.Client.Payment;
+using MercadoPago.Config;
+using MercadoPago.Resource.Payment;
+using Microsoft.Extensions.Configuration;
+using venue_service.Src.Interfaces.Payment;
+
+namespace venue_service.Src.Services.Payment
+{
+    public class MercadoPagoPaymentService : IPaymentService
+    {
+        public MercadoPagoPaymentService(IConfiguration config)
+        {
+            MercadoPagoConfig.AccessToken = config["MercadoPago:AccessToken"];
+        }
+
+        public async Task<(string status, string mercadoPagoId)> CreatePaymentAsync(string email, string cardToken, decimal amount, string description)
+        {
+            var paymentRequest = new PaymentCreateRequest
+            {
+                TransactionAmount = amount,
+                Token = cardToken,
+                Description = description,
+                Installments = 1,
+                PaymentMethodId = "visa",
+                Payer = new PaymentPayerRequest { Email = email }
+            };
+
+            var client = new PaymentClient();
+            var result = await client.CreateAsync(paymentRequest);
+
+            return (result.Status.ToString(), result.Id.ToString());
+        }
+    }
+}
