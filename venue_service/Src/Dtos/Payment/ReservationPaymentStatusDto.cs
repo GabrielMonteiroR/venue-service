83c96f0344c48ddfcc1687487ac7d630ffda60a9
Gabriel <gmonteiro810@gmail.com>
2025-05-22 19:36:13 -0300

    Add ReservationPaymentStatusDto class for payment status
    Introduces a new class `ReservationPaymentStatusDto` in the
    `venue_service.Src.Dtos.Payment` namespace. This class contains
    four properties: `ReservationId`, `HasPayment`, `PaymentStatus`,
    and `PaidAt`, to represent the payment status of a reservation.

---
+++ venue_service/Src/Dtos/Payment/ReservationPaymentStatusDto.cs
@@ -0,0 +1,10 @@
+﻿namespace venue_service.Src.Dtos.Payment
+{
+    public class ReservationPaymentStatusDto
+    {
+        public int ReservationId { get; set; }
+        public bool HasPayment { get; set; }
+        public string? PaymentStatus { get; set; }
+        public DateTime? PaidAt { get; set; }
+    }
+}
