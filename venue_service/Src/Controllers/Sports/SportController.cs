cdf8460371340e9cce23f856facac814569351c4
Gabriel <gmonteiro810@gmail.com>
2025-06-05 20:07:51 -0300

    Add ISportInterface dependency to SportController
    Updated SportController to use ISportInterface for dependency injection. Added asynchronous method GetAllSports to retrieve sports data. Updated namespace and route attributes for improved structure.

--- venue_service/Src/Controllers/Sports/SportController.cs
+++ venue_service/Src/Controllers/Sports/SportController.cs
@@ -1,8 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using venue_service.Src.Interfaces.SportInterfaces;
 
-namespace venue_service.Src.Controllers.Sports
+namespace venue_service.Src.Controllers.Sports;
+
+[ApiController]
+[Route("api/sports")]
+public class SportController : ControllerBase
 {
-    public class SportController : ControllerBase
+    private readonly ISportInterface _sportInterface;
+
+    public SportController(ISportInterface sportInterface)
     {
+        _sportInterface = sportInterface;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllSports()
+    {
+        var result = await _sportInterface.GetAllSportsAsync();
+        return Ok(result);
     }
 }
